ce9720468d1660716643b2a9f6e3f2e4192b1400
Adrijana1308 <adrijanababic0602@gmail.com>
2022-10-11 00:42:16 +0200

    Rješen zadatak 3

---
+++ 01-zadaci/zadatak3.js
@@ -0,0 +1,9 @@
+ var func2 = function (x){
+    if (x >= 100 && x <= 150000){
+        console.log(x, " se nalazi unutar intervala")
+    } else {
+        console.log(x, " se ne nalazi unutar intervala")
+    }
+ }
+
+ func2(456)
