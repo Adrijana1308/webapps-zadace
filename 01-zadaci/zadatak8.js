62f9da6ab79a661baeb41ba6624f9371f74953ec
Adrijana1308 <adrijanababic0602@gmail.com>
2022-10-11 20:26:35 +0200

    Rjesen zadatak 8

---
+++ 01-zadaci/zadatak8.js
@@ -0,0 +1,11 @@
+/*Napiši funkciju koja vraća predano
+ polje obrnutim redosljedom*/
+ 
+ var polje = [40, 100, 1, 5, 25, 10];
+
+ var func8 = function (polje){
+    polje.reverse()
+    return polje
+ }
+
+ console.log(func8(polje))
